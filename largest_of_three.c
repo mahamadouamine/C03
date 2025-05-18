d5ed687eaf381bfb9ae8842c4579356cf0408873
Mahamadou Amine <mahamadouamine@iMac-de-Souleymane.local>
2025-05-18 19:18:12 +0100

    com

--- largest_of_three.c
+++ largest_of_three.c
@@ -1,21 +1,23 @@
-#include<stdio.h>
-int main()
+#include<stdio.h>// Inclusion de la bibliothèque standard pour l'entrée/sortie (printf, scanf)
+int main()// Début du programme
 {
-    int x,y,z;
-    int resultat;
+    int x,y,z;// Déclaration de trois variables entières pour stocker les nombres entrés
+    int resultat;// Variable pour stocker le plus grand des trois nombres
 
-    scanf("%d %d %d", &x, &y, &z);
+    scanf("%d %d %d", &x, &y, &z);// Lecture des trois entiers entrés par l'utilisateur
     //pour commencer , on suppose que x est le plus grand nombre
-    resultat = x;
+    resultat = x; //initialisation de resultat = x
 
-    if (y > resultat)
+
+    if (y > resultat)  // Si y est plus grand que le resultat actuel (x)
   {
-        resultat = y;
+        resultat = y;// Alors on met à jour resultat avec la valeur de y
   }
-    if (z > resultat)
+    if (z > resultat)// Si z est plus grand que le resultat actuel
   {
-        resultat = z;
+        resultat = z; // Alors on met à jour resultat avec la valeur de z
   }
+     // Affichage du plus grand nombre trouvé
     printf("Le plus grand nombre est : %d\n", resultat);
-    return 0;
+    return 0;// Fin du programme
 }
