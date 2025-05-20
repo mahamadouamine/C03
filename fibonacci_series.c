e40e3aff69e15cfd8dd19bd0503e1ec4206d1cb9
Mahamadou Amine <mahamadouamine@iMac-de-Souleymane.local>
2025-05-20 12:22:07 +0100

    com

---
+++ fibonacci_series.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int limit;
+    int a = 0, b = 1, temp;
+    //lecture de la limite
+    do{
+        scanf("%d", &limit);
+    }while(limit < 2);
+    //affichage de la suite de fibonacci jusqu'à la limite
+    printf("%d ", a);
+    while (b <= limit)
+    {
+        printf("%d ",b);
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    printf("\n");
+    return 0;
+}
